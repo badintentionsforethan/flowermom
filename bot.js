385ef24bf87655e613ae12c94f00908a295a3443
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-01-29 14:41:31 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,26 +5,23 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-var mine;
-var buy;
-
-function step1(){
-	message.channel.send('!mine');
-	message.channel.send('!collect');
-	var buy = setInterval(function (){step2(1)}, 30000);
-	}
-function step2(){	
-	message.channel.send('!buy max fracker');
-	message.channel.send('!buy max swarm');
-	message.channel.send('!buy max robot');
-	message.channel.send('!buy max miner');
-	message.channel.send('!prestige YESIMSURE');
-	}
 
 client.on('message', message =>{
  if (message.author.id == 260241663018926080 && message.content.includes("!start")) {
 	message.react("👌");  //Acknowledge
 	var mine = setInterval(function(){step1(1)}, 3000);
+	}
+	function step1(){
+	message.channel.send('!mine');
+	message.channel.send('!collect');
+	var buy = setInterval(function (){step2(1)}, 30000);
+	}
+	function step2(){	
+	message.channel.send('!buy max fracker');
+	message.channel.send('!buy max swarm');
+	message.channel.send('!buy max robot');
+	message.channel.send('!buy max miner');
+	message.channel.send('!prestige YESIMSURE');
 	}
 });
 
@@ -37,6 +34,7 @@ clearInterval(buy);
 clearInterval(mine);
 	}
 });
+
 
 
 // THIS  MUST  BE  THIS  WAY
