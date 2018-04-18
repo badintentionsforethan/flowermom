11c112caa5bdccf00dc5480f76bdea523052456e
badintentionsforethan <38505202+badintentionsforethan@users.noreply.github.com>
2018-04-18 10:12:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,108 +14,29 @@ function strt () {
 function routine1(){
 	function step1(){
 	message.channel.send('!mine');
-	message.channel.send('!collect');
-	message.channel.send('!memes');
 	}
 	function step2(){
-	message.channel.send('!buy max fracker');
+	message.channel.send('!collect');
 	}
 	function step3(){
-	message.channel.send('!buy max swarm');
-	}
-	function step4(){	
-	message.channel.send('!buy max robot');
-	}
-	function step5(){	
-	message.channel.send('!buy max miner');
-	message.channel.send('!prestige YESIMSURE');
-	}
-	var one = setTimeout(function() {step1(1)}, 500);
-	var two = setTimeout(function() {step2(1)}, 30000);
-	var three = setTimeout(function() {step3(1)}, 40000);
-	var four = setTimeout(function() {step4(1)}, 50000);
-	var five = setTimeout(function() {step5(1)}, 60000);
-	}
-function routine2(){
-	function step1(){
-	message.channel.send('!mine');
-	message.channel.send('!collect');
 	message.channel.send('!memes');
 	}
-	function step2(){	
-	
-	}
-	
-	function step3(){
-	message.channel.send('!buy max swarm');
-	}
 	function step4(){	
-	message.channel.send('!buy max robot');
+	message.channel.send('!buy max miner');
 	}
 	function step5(){	
-	message.channel.send('!buy max miner');
-	message.channel.send('!prestige YESIMSURE');
+		message.channel.send('!prestige YESIMSURE');
 	}
-	var one = setTimeout(function() {step1(1)}, 500);
-	//var two = setTimeout(function() {step2(1)}, 30000);
-	var three = setTimeout(function() {step3(1)}, 40000);
-	var four = setTimeout(function() {step4(1)}, 50000);
-	var five = setTimeout(function() {step5(1)}, 60000);
+	var one = setTimeout(function() {step1(1)}, 200);
+	var two = setTimeout(function() {step2(1)}, 3000);
+	var three = setTimeout(function() {step3(1)}, 5000);
+	var four = setTimeout(function() {step4(1)}, 7000);
+	var five = setTimeout(function() {step5(1)}, 9000);
 	}
-function routine3(){
-	function step1(){
-	message.channel.send('!mine');
-	message.channel.send('!collect');
-	message.channel.send('!memes');
-	}
-	function step2(){	
-	}
-	function step3(){
-	}
-	function step4(){
-	message.channel.send('!buy max robot');
-	}
-	function step5(){	
-	message.channel.send('!buy max miner');
-	message.channel.send('!prestige YESIMSURE');
-	}
-	var one = setTimeout(function() {step1(1)}, 500);
-	//var two = setTimeout(function() {step2(1)}, 30000);
-	//var three = setTimeout(function() {step3(1)}, 40000);
-	var four = setTimeout(function() {step4(1)}, 50000);
-	var five = setTimeout(function() {step5(1)}, 60000);
-	}
-function routine4(){
-	function step1(){
-	message.channel.send('!mine');
-	message.channel.send('!collect');
-	message.channel.send('!memes');
-	}
-	function step2(){	
-	message.channel.send('!buy max fracker');
-	}
-	function step3(){
-	message.channel.send('!buy max swarm');
-	}
-	function step4(){	
-	}
-	function step5(){
-	message.channel.send('!buy max miner');
-	message.channel.send('!prestige YESIMSURE');
-	}
-	var one = setTimeout(function() {step1(1)}, 500);
-	//var two = setTimeout(function() {step2(1)}, 30000);
-	//var three = setTimeout(function() {step3(1)}, 40000);
-	//var four = setTimeout(function() {step4(1)}, 50000);
-	var five = setTimeout(function() {step5(1)}, 60000);
-	}
-var routineA = setTimeout(function() {routine1(1)}, 1000);
-var routineB = setTimeout(function() {routine2(1)}, 81000);
-var routineC = setTimeout(function() {routine3(1)}, 161000);
-var routineD = setTimeout(function() {routine4(1)}, 241000);
+
+var routineA = setTimeout(function() {routine1(1)}, 200);
 		}
-	var initial1 = setTimeout(function() {strt(1)}, 200);
-	var initial2 = setInterval(function() {strt(1)}, 480000);
+	var initial2 = setInterval(function() {strt(1)}, 11000);
  }
 });
 //________________________________________________________________________________________________________________________
@@ -124,3 +45,7 @@ var routineD = setTimeout(function() {routine4(1)}, 241000);
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN2);
+client.login(process.env.BOT_TOKEN3);
+client.login(process.env.BOT_TOKEN4);
+client.login(process.env.BOT_TOKEN5);
