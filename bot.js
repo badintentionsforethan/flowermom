66b9eb016046a611a7145aaaf1be4368282e6cd5
badintentionsforethan <38505202+badintentionsforethan@users.noreply.github.com>
2018-04-20 09:24:57 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,29 +14,108 @@ function strt () {
 function routine1(){
 	function step1(){
 	message.channel.send('!mine');
-	}
-	function step2(){
 	message.channel.send('!collect');
-	}
-	function step3(){
 	message.channel.send('!memes');
 	}
+	function step2(){
+	message.channel.send('!buy max fracker');
+	}
+	function step3(){
+	message.channel.send('!buy max swarm');
+	}
 	function step4(){	
-	message.channel.send('!buy max miner');
+	message.channel.send('!buy max robot');
 	}
 	function step5(){	
-		message.channel.send('!prestige YESIMSURE');
+	message.channel.send('!buy max miner');
+	message.channel.send('!prestige YESIMSURE');
 	}
-	var one = setTimeout(function() {step1(1)}, 200);
-	var two = setTimeout(function() {step2(1)}, 3000);
-	var three = setTimeout(function() {step3(1)}, 5000);
-	var four = setTimeout(function() {step4(1)}, 7000);
-	var five = setTimeout(function() {step5(1)}, 9000);
+	var one = setTimeout(function() {step1(1)}, 500);
+	var two = setTimeout(function() {step2(1)}, 30000);
+	var three = setTimeout(function() {step3(1)}, 40000);
+	var four = setTimeout(function() {step4(1)}, 50000);
+	var five = setTimeout(function() {step5(1)}, 60000);
 	}
-
-var routineA = setTimeout(function() {routine1(1)}, 200);
+function routine2(){
+	function step1(){
+	message.channel.send('!mine');
+	message.channel.send('!collect');
+	message.channel.send('!memes');
+	}
+	function step2(){	
+	
+	}
+	
+	function step3(){
+	message.channel.send('!buy max swarm');
+	}
+	function step4(){	
+	message.channel.send('!buy max robot');
+	}
+	function step5(){	
+	message.channel.send('!buy max miner');
+	message.channel.send('!prestige YESIMSURE');
+	}
+	var one = setTimeout(function() {step1(1)}, 500);
+	//var two = setTimeout(function() {step2(1)}, 30000);
+	var three = setTimeout(function() {step3(1)}, 40000);
+	var four = setTimeout(function() {step4(1)}, 50000);
+	var five = setTimeout(function() {step5(1)}, 60000);
+	}
+function routine3(){
+	function step1(){
+	message.channel.send('!mine');
+	message.channel.send('!collect');
+	message.channel.send('!memes');
+	}
+	function step2(){	
+	}
+	function step3(){
+	}
+	function step4(){
+	message.channel.send('!buy max robot');
+	}
+	function step5(){	
+	message.channel.send('!buy max miner');
+	message.channel.send('!prestige YESIMSURE');
+	}
+	var one = setTimeout(function() {step1(1)}, 500);
+	//var two = setTimeout(function() {step2(1)}, 30000);
+	//var three = setTimeout(function() {step3(1)}, 40000);
+	var four = setTimeout(function() {step4(1)}, 50000);
+	var five = setTimeout(function() {step5(1)}, 60000);
+	}
+function routine4(){
+	function step1(){
+	message.channel.send('!mine');
+	message.channel.send('!collect');
+	message.channel.send('!memes');
+	}
+	function step2(){	
+	message.channel.send('!buy max fracker');
+	}
+	function step3(){
+	message.channel.send('!buy max swarm');
+	}
+	function step4(){	
+	}
+	function step5(){
+	message.channel.send('!buy max miner');
+	message.channel.send('!prestige YESIMSURE');
+	}
+	var one = setTimeout(function() {step1(1)}, 500);
+	//var two = setTimeout(function() {step2(1)}, 30000);
+	//var three = setTimeout(function() {step3(1)}, 40000);
+	//var four = setTimeout(function() {step4(1)}, 50000);
+	var five = setTimeout(function() {step5(1)}, 60000);
+	}
+var routineA = setTimeout(function() {routine1(1)}, 1000);
+var routineB = setTimeout(function() {routine2(1)}, 81000);
+var routineC = setTimeout(function() {routine3(1)}, 161000);
+var routineD = setTimeout(function() {routine4(1)}, 241000);
 		}
-	var initial2 = setInterval(function() {strt(1)}, 11000);
+	var initial1 = setTimeout(function() {strt(1)}, 200);
+	var initial2 = setInterval(function() {strt(1)}, 480000);
  }
 });
 //________________________________________________________________________________________________________________________
